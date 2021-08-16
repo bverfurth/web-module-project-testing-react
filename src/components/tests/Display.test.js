c5de9a3aac1cfb2670bfc466ae54eccdfbbba0b7
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-16 16:26:01 -0400

    Finished display tests. all passing. mvp complete

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,7 +1,63 @@
+// Add in nessisary imports
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Display from "../Display";
+import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+
+// Add In Necessary Values To Establish The Testing Suite
+const testShow = {
+  name: "test show",
+  summary: "what a show",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+    { id: 2, name: "Season 3", episodes: [] },
+  ],
+};
+
+// Test That The Display Component Renders Without Any Passed In Props
+test("renders display component without any props passed in", () => {
+  render(<Display />);
+});
+
+// Rebuild Or Copy A Show Test Data Element As Used In The Previous Set Of Tests
+test("when the fetch button is pressed, the show component will display", async () => {
+  render(<Display show={testShow} />);
+
+  await waitFor(() => {
+    const show = screen.queryAllByTestId("show-container");
+    expect(show).toBeTruthy();
+  });
+
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
+});
+
+// Test That When The Fetch Button Is Pressed, The Show Component Will Display
+test("when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data", async () => {
+  render(<Display show={testShow} />);
+
+  await waitFor(() => {
+    const show = screen.queryAllByTestId("show-container");
+    expect(show).toBeTruthy();
+  });
+
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
+});
+
+// Test That When The Fetch Button Is Pressed, The Amount Of Select Options Rendered Is Equal To The Amount Of Seasons In Your Test Data
+test("when the fetch button is pressed, this function is called", async () => {
+  render(<Display show={testShow} />);
+
+  await waitFor(() => {
+    const show = screen.queryAllByTestId("show-container");
+    expect(show).toBeTruthy();
+  });
+
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
